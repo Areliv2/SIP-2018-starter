d670dcb86135736f7d6eddd50b36259f62535ee6
Areli Vargas <areliv2@pucschools.net>
2019-07-01 14:51:13 -0700

    data project

--- U2-Applications/U2.1-Data/data_vis_project_starter.py
+++ U2-Applications/U2.1-Data/data_vis_project_starter.py
@@ -13,8 +13,40 @@ tweetFile = open("TwitterData/tweets_small.json", "r")
 tweetData = json.load(tweetFile)
 tweetFile.close()
 
+
 # Continue your program below! 
+tweettext = []
+tweetstring = ""
+for tweet in tweetData:
+    tweetstring += tweet['text']
+    #blob = TextBlob(tweet["text"])
+    #tweettext.append(blob)
+    #print(tweetstring)
+
+my_list = [1,2,3,4,5,6]  
+print(sum(my_list)/ len(my_list)) 
+
+tweetBlob = TextBlob(tweetstring)
+print(tweetBlob.translate(to='es'))
+
+
+
+blob_polarity = []
+for blob in tweettext:
+    blob_polarity.append(blob.polarity)
+
+word_dict= {}
+'FilteredWord[word] = count'
+
+for word in tweetBlob.words:
+    word_dict[word.lower()] = tweetBlob.word_counts[word.lower()]
+
+blob_subjectivity = []
+for blob in tweettext:
+    blob_subjectivity.append(blob.subjectivity)
+    
+print(word_dict) 
+
 
 # Textblob sample:
 tb = TextBlob("You are a brilliant computer scientist.")
-print(tb.polarity)
